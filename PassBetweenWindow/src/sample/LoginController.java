1c75351b86ca2cabf2900a7026cb6ca78ad5923b
Natsu3773 <matthewjames0512@gmail.com>
2021-05-19 20:01:40 +0200

    Adding Windows per Task Schedule

--- PassBetweenWindow/src/sample/LoginController.java
+++ PassBetweenWindow/src/sample/LoginController.java
@@ -1,12 +1,16 @@
 package sample;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class LoginController {
+@FXML Label label;
 
     public void onLoginClick(ActionEvent actionEvent) {
         /*
@@ -14,6 +18,7 @@ public class LoginController {
             If valid credentials: move the try catch block inside the if block. Else consume the event.
             Assume valid credentials for navigation testing.
          */
+
         try {
             // Gets the current window and controls and closes it.
             Node source = (Node)  actionEvent.getSource();
@@ -21,14 +26,13 @@ public class LoginController {
             stage.close();
             FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
             Parent parent = loader.load();
-            Controller controller = loader.getController();
-            controller.setLoginStatus("true");
-
+            Controller con = loader.getController();
+            con.setLoginStatus();
+            // Defensively consume event
+            actionEvent.consume();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //Else  Defensively consume event
-        actionEvent.consume();
     }
 
     public void onCancelClick(ActionEvent actionEvent) {
